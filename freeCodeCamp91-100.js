8e3843f47ec6fdd6682976341a7408ddad6a2306
Ivan Stabronik <engel.stabronik@gmail.com>
2022-04-25 11:38:58 +0300

    second commit

---
+++ freeCodeCamp91-100.js
@@ -0,0 +1,117 @@
+// task 91
+// Setup
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+};
+delete myDog.tails;
+// Only change code below this line
+
+// task 92
+// Setup
+function phoneticLookup(val) {
+    let result = "";
+
+    // Only change code below this line
+    const lookup = {
+        "alpha": "Adams",
+        "bravo": "Boston",
+        "charlie": "Chicago",
+        "delta": "Denver",
+        "echo": "Easy",
+        "foxtrot": "Frank"
+    }
+    result = lookup[val];
+    // Only change code above this line
+    return result;
+}
+
+phoneticLookup("charlie");
+
+// task 93
+function checkObj(obj, checkProp) {
+    // Only change code below this line
+    if (obj.hasOwnProperty(checkProp)) {
+        return obj[checkProp];
+    } else {
+        return "Not Found";
+    }
+    // Only change code above this line
+}
+
+// task 94
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "The Cure",
+        "title": "Faith",
+        "release_year": 1981,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": false
+    }
+];
+
+// task 95
+const myStorage = {
+    "car": {
+        "inside": {
+            "glove box": "maps",
+            "passenger seat": "crumbs"
+        },
+        "outside": {
+            "trunk": "jack"
+        }
+    }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
+
+// task 96
+const myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+];
+
+const secondTree = myPlants[1].list[1];
+
+// task 97
+
+
+// task 98
+
+
+// task 99
+
+
+// task 100
