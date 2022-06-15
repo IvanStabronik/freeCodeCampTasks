f28256e45c344c32b3cdff7291fbb35c3964b06c
Ivan Stabronik <engel.stabronik@gmail.com>
2022-06-15 15:11:11 +0300

    fifth commit

--- freeCodeCamp111-120.js
+++ freeCodeCamp111-120.js
@@ -40,11 +40,60 @@ function countdown(n){
 // Only change code above this line
 
 //task 116
+function rangeOfNumbers(startNum, endNum) {
+    if (endNum - startNum === 0) {
+        return [startNum];
+    } else {
+        let numbers = rangeOfNumbers(startNum, endNum - 1)
+        numbers.push(endNum);
+        return numbers;
+    }
+};
 
 //task 117
+function checkScope() {
+    let i = 'function scope';
+    if (true) {
+        let i = 'block scope';
+        console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+}
 
 //task 118
+const s = [5, 7, 2];
+function editInPlace() {
+    // Only change code below this line
+
+    s[0] = 2;
+    s[1] = 5;
+    s[2] = 7;
+    // Using s = [2, 5, 7] would be invalid
+
+    // Only change code above this line
+}
+editInPlace();
 
 //task 119
+function freezeObj() {
+    const MATH_CONSTANTS = {
+        PI: 3.14
+    };
+    // Only change code below this line
+    Object.freeze(MATH_CONSTANTS);
+
+    // Only change code above this line
+    try {
+        MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+        console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
 
 //task 120
+const magic = () => {
+    return new Date();
+};
