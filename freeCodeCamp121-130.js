2c6e2f6755a7e288c2b73a82012e4ede4b81aee2
Ivan Stabronik <engel.stabronik@gmail.com>
2022-06-15 15:48:43 +0300

    sixth commit

---
+++ freeCodeCamp121-130.js
@@ -0,0 +1,27 @@
+//task 121
+const myConcat = (arr1, arr2) => {
+    return arr1.concat(arr2);
+};
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+//task 122
+// Only change code below this line
+const increment = (number, value = 1) => number + value;
+// Only change code above this line
+
+//task 123
+
+//task 124
+
+//task 125
+
+//task 126
+
+//task 127
+
+//task 128
+
+//task 129
+
+//task 130
